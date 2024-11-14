0c4745a0f8015c1b8c9fb233e692b0b88aea1d08
Pourya Zardosht <pooriyazardosht.812@gmail.com>
2024-11-14 20:14:46 +0330

    homepage header created

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { Route, Routes } from "react-router-dom"
-import HomePage from "./components/templates/HomePage"
+import HomePage from "./pages/home/HomePage";
 
 function App() {
   return (
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,9 +2,22 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
+    fontSize: {
+      sm: "0.8rem",
+      base: "1rem",
+      xl: "1.25rem",
+      "2xl": "1.5rem",
+      "3xl": "2rem",
+      "4xl": "2.441rem",
+      "5xl": "3.052rem",
+    },
+    container: {
+      center: true,
+    },
     extend: {
       colors: {
         bgColor: "#F7F8F8",
+        primaryColor: "#F21055",
       },
       fontFamily: {
         vazir: ["vazirmatn", "sans-serif"],
