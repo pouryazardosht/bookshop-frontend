5e5101011d34bf33925043b2cafb21c90052995c
Pourya Zardosht <pooriyazardosht.812@gmail.com>
2024-11-28 00:43:35 +0330

    fix: loader spinner add (need fix)

--- src/pages/home/components/cards/CardsContainer.jsx
+++ src/pages/home/components/cards/CardsContainer.jsx
@@ -1,3 +1,4 @@
+import { Oval } from "react-loader-spinner"
 import { useBooks } from "../../../../context/BookContext"
 import Card from "./Card"
 
@@ -6,11 +7,25 @@ function CardsContainer() {
     const { books } = useBooks()
     const result = books.data
     return (
-        <div className="flex justify-center my-6">
+        <div className=" flex justify-center my-6">
+
             <div className="grid gap-8 grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 ">
                 {result ? result.map((book) => (
                     <Card key={book.id} book={book} />
-                )) : <h1>Loading</h1>}
+                )) :
+                    <div className="h-screen flex items-center justify-center">
+
+                        <Oval
+                            visible={true}
+                            height="80"
+                            width="80"
+                            color="#F21055"
+                            ariaLabel="oval-loading"
+                            wrapperStyle={{}}
+                            wrapperClass=""
+                        />
+                    </div>
+                }
             </div>
         </div >
     )
